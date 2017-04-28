e26a6328332d974e3a22d4c65c69e23c4ae9afe7
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2017-04-28 17:03:35 -0500

    updated database design

--- +seq/Genome.m
+++ +seq/Genome.m
@@ -2,20 +2,20 @@
 # genome assembly used for alignment
 assembly: varchar(20) # short name of genome assembly
 -----
-species="":     varchar(20)     #short name of species, if applicable
+-> seq.Species
+source_location = ''  : varchar(255)  # a file or ftp or 
 %}
 
 classdef Genome < dj.Lookup
-    methods
-        function fill(self)
-            self.inserti({
-                'hg19' 'human'
-                'hg38' 'human'
-                'mm9' 'mouse'
-                'mm10' 'mouse'
-                'RFP' ''
-                'eGFP' ''
-            })
-        end
+    
+    properties
+        contents = {
+            'hg19' 'human'  'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/'
+            'hg38' 'human'  'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/'
+            'mm9'  'mouse'  'ftp://hgdownload.soe.ucsc.edu/goldenPath/mm9/chromosomes/'
+            'mm10' 'mouse'  'ftp://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/'
+            'RFP'  '' ''
+            'eGFP' '' ''
+            }
     end
 end
