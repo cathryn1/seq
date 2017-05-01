b47b7eb2bfc47223378db5c8953a1b72340903eb
cathryn1 <crhughes@bcm.edu>
2017-05-01 15:46:49 -0500

    /  Please enter the commit message for your changes. Lines starting

--- +seq/FlowCell.m
+++ +seq/FlowCell.m
@@ -1,20 +1,9 @@
 %{
-# 
--> seq.Run
----
+# Flow Cell used
 flowcell                    : varchar(255)                  # flowcell used on this run
-run_date                    : date                          # run date
 %}
 
 
-classdef FlowCell < dj.Imported
-
-	methods(Access=protected)
-
-		function makeTuples(self, key)
-            error 'this table is populated in python'
-			 self.insert(key)
-		end
-	end
+classdef FlowCell < dj.Manual
 
 end
--- +seq/Run.m
+++ +seq/Run.m
@@ -2,6 +2,7 @@
 # information about each sequencing run
 run_id                      : varchar(20)             # unique id for each run
 ---
+->seq.FlowCell
 file_pattern                : varchar(255)                  # data folder for the filede
 machine                     : varchar(25)                   # machine used for sequencing, i.e. HiSeq 2500
 %}
